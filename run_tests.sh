cdd431283c264e8e06afb22a388595b93d42ca88
Steve Farrell <sparticlesteve@gmail.com>
2024-10-28 15:30:53 -0700

    update libfabric path

--- run_tests.sh
+++ run_tests.sh
@@ -12,7 +12,7 @@
 WORKDIR=$SCRATCH/podman-nccl-tests/run_tests #_$SLURM_JOB_ID
 CONT_NAME=nersc/pytorch:24.06.01
 NCCL_NET_PATH=/global/common/software/nersc9/nccl/2.19/plugin
-LIBFABRIC_PATH=/opt/cray/libfabric/1.15.2.0/lib64
+LIBFABRIC_PATH=/opt/cray/libfabric/1.20.1/lib64
 export FI_MR_CACHE_MONITOR=userfaultfd
 export NCCL_DEBUG=INFO
 export FI_LOG_LEVEL=debug
